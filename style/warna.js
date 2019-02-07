37be1048a33dcb2e276ff8bd912bec187aad035d
Ibnu syawal <46734417+ibnusyawall@users.noreply.github.com>
2019-02-08 01:12:45 +0700

    Create warna.js

---
+++ style/warna.js
@@ -0,0 +1,13 @@
+/* Created By Ibnu Syawal */
+/* Say Alhamdulillah :) */
+
+var warna = require('colors');    // npm install --save colors
+
+/* ........................................ */
+warna.setTheme({
+    hijau : 'green',
+    biru : 'blue',
+    merah : 'red',
+    kuning : 'yellow'
+});
+/* ........................................ */
